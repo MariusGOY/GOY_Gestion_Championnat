06d0f0350d7dd7ebb966b35e1871ccb6d66e8d1b
MariusGOY <marius.goy2002@gmail.com>
2024-03-22 22:45:02 +0100

    MG : comique initiale

---
+++ src/main/java/com/blog/goy_gestion_championnat/GoyGestionChampionnatApplication.java
@@ -0,0 +1,13 @@
+package com.blog.goy_gestion_championnat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoyGestionChampionnatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoyGestionChampionnatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blog/goy_gestion_championnat/GoyGestionChampionnatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.goy_gestion_championnat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoyGestionChampionnatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
